6c8aee3ba567f09ba23bac6e6d6b15a69a21e883
oliver <oliverlcruickshank@gmail.com>
2018-01-31 23:22:11 +0000

    Readded ability to load objects at runtime

--- src/engine.cpp
+++ src/engine.cpp
@@ -15,9 +15,11 @@ void Engine::restart()
     mats.push_back(std::make_shared<Lambertian>(rand_num));
     mats.push_back(std::make_shared<Lambertian>(float3(1.0, 0.0, 0.0)));
     
-    for (int face = 0; face < mesh.size(); face++)
-        //m_data[face]->setMaterial(mats[0]);
-        m_screen->reset();
+    //for (int face = 0; face < mesh.size(); face++)
+    //m_data[face]->setMaterial(mats[0]);
+    m_screen->reset();
+    std::string obj_open = m_screen->obj_to_open;
+    mesh.load(obj_open);
 }
 
 void Engine::loadBuffer(std::vector<float3> image)
@@ -34,6 +36,7 @@ void Engine::loadBuffer(std::vector<float3> image)
 }
 void Engine::render()
 {
+    //omp_set_num_threads(3);
     HitInfo hit;
     std::vector<float3> temp_img;
     temp_img.resize(m_screen->getWidth() * m_screen->getHeight());
@@ -43,7 +46,7 @@ void Engine::render()
     float3 colour = float3();
     float u, v;
     const int tile_size = 80;
-#pragma omp parallel for schedule(static) private(hit, persp, ortho, colour, u, v) collapse(2)
+#pragma omp parallel for schedule(dynamic, 1) private(hit, persp, ortho, colour, u, v) collapse(2)
     for (int tx = 0; tx < m_screen->getWidth() / tile_size; tx++) 
     {
         for (int ty = 0; ty < m_screen->getHeight() / tile_size; ty++)
@@ -58,6 +61,7 @@ void Engine::render()
                     v = float(y + ty*tile_size + drand48()) / m_screen->getHeight();
                     persp = m_cam.getRay(u, v);
                     colour = colour + trace(persp, hit, 0);
+                    
                     temp_img[x + tx*tile_size + (ty*tile_size + y) * m_screen->getWidth()] = colour;
                 }
             }
@@ -77,7 +81,7 @@ float3 Engine::trace(Ray &ray, HitInfo &hit, int depth)
     {
         Ray new_ray;
         float3 col(1.0, 1.0, 1.0);
-        float3 light = hit.mat->emitted();
+        float3 light = hit.mat->emitted(hit);
         if (depth < 10 && hit.mat->scatter(ray, hit, col, new_ray))
         {
             return light + col * trace(new_ray, hit, depth + 1);
--- src/screen.h
+++ src/screen.h
@@ -9,21 +9,21 @@
 
 class Screen
 {
-  public:
+    public:
     Screen(int width, int height) : m_width(width), m_height(height),
-                                    sample(1)
+    sample(1)
     {
         m_pixelBuffer.resize(width * height);
         m_img.create(width, height, sf::Color::Black);
         m_tex.loadFromImage(m_img);
         should_reset = false;
         m_win_states.save = false;
-		m_win_states.colour = false;
-		debug_text.move(sf::Vector2f(20, height - 40));
-		debug_text.scale(sf::Vector2f(0.8,0.8));
-
-		Font.loadFromFile("../../res/fonts/helvetica.ttf");
-
+        m_win_states.colour = false;
+        debug_text.move(sf::Vector2f(20, height - 40));
+        debug_text.scale(sf::Vector2f(0.8,0.8));
+        
+        Font.loadFromFile("../../res/fonts/helvetica.ttf");
+        
     };
     ~Screen();
     void setPixel(int x, int y, float3 colour);
@@ -35,23 +35,25 @@ class Screen
     void drawUI();
     void loadImage(std::vector<float3> img);
     void reset();
-	sf::Text debug_text;
-	sf::Font Font;
-	int sample;
-  private:
+    sf::Text debug_text;
+    sf::Font Font;
+    int sample;
+    char obj_to_open[64];
+    private:
     float3 avg(float3 current_avg, float3 new_colour);
     sf::Color transform(float3 pixel);
     struct Window_states
     {
         bool save;
-		bool colour;
+        bool colour;
+        bool open;
     };
-	sf::Clock clock;
+    sf::Clock clock;
     std::vector<float3> m_pixelBuffer;
     sf::Image m_img;
     char m_img_name[32];
     sf::Texture m_tex;
-	Window_states m_win_states;
+    Window_states m_win_states;
     bool should_reset;
     float m_color[3] = {1.0f, 1.0f, 1.0f};
     int m_width;

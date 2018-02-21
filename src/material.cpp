9967d4cbf40ac75b058709e9f8ba58a49d4e2a13
oliver <oliverlcruickshank@gmail.com>
2018-02-21 23:22:52 +0000

    Added orthonormal basis and fixed the cosine hemi sampling

--- src/material.cpp
+++ src/material.cpp
@@ -9,30 +9,33 @@ float fresnel(float ior, float ndotv) {
 
 float3 cosineSampleHemi(float u1, float u2)
 {
-    float z = std::pow(1.0 - u1, 3.0 / 5.0);
-    float phi = 2.0 * M_PI * u2;
-    float theta = sqrt(std::max(0.0, 1.0 - z * z));
-    float x = theta * cos(phi);
-    float y = theta * sin(phi);
-    float offset = drand48()/1000.0f;
-    return float3(x + offset, y, z + offset);
+    
+    float z = sqrt(1-u2);
+    float phi = 2*M_PI*u1;
+    float x = cos(phi)*2*sqrt(u2);
+    float y = sin(phi)*2*sqrt(u2);
+    return float3(x, y, z);
+    
 }
 
-bool Emissive::scatter(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) const
+bool Emissive::BSDF(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) const
 {
-    attenuation = float3(hit.normal.x,hit.normal.y, hit.normal.z);
+    //attenuation = float3(hit.normal.x,hit.normal.y, hit.normal.z);
     return false;
 }
 
-bool Lambertian::scatter(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) const
+bool Lambertian::BSDF(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) const
 {
     float3 offset = hit.normal*(1e-5);
-    new_ray = Ray(ray.getHit(hit.t) + offset, cosineSampleHemi(drand48(), drand48()));
+    float u1 = drand48(); float u2 = drand48();
+    hit.costheta = u1;
+    new_ray = Ray(ray.getHit(hit.t) + offset, make_orthonormal_basis(cosineSampleHemi(u1, u2), hit.normal));
+    
     attenuation = sample_texture(texture, hit.u, hit.v);
     return true;
 }
 
-bool Mirror::scatter(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) const
+bool Mirror::BSDF(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) const
 {
     float3 r = float3(drand48(), drand48(), drand48())*glossiness;
     new_ray = Ray(ray.getHit(hit.t), ray.getDirection() - hit.normal * 2.0 * ray.getDirection().dot(hit.normal) + r);
@@ -40,20 +43,20 @@ bool Mirror::scatter(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) 
     return true;
 }
 
-bool Mix::scatter(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) const
+bool Mix::BSDF(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) const
 {
     float3 att1, att2;
     Ray nray1, nray2;
     float f = fresnel(mix, hit.normal.dot(unit(ray.getDirection()*(-1))));
-    mat1->scatter(ray, hit, att1, nray1);
-    mat2->scatter(ray, hit, att2, nray2);
+    mat1->BSDF(ray, hit, att1, nray1);
+    mat2->BSDF(ray, hit, att2, nray2);
     if (f > 1.0) f = 1.0f;
     if (drand48() > f) new_ray = nray1; else new_ray = nray2;
     attenuation = (att2* f) + (att1 * (1.0f - f));
     return true;
 }
 
-bool Glass::scatter(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) const
+bool Glass::BSDF(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) const
 {
     float3 r = float3(drand48(), drand48(), drand48())*glossiness;
     new_ray = Ray(ray.getHit(hit.t), ray.getDirection() - hit.normal * 2.0 * ray.getDirection().dot(hit.normal*(-1)) + r);
--- src/material.h
+++ src/material.h
@@ -8,19 +8,34 @@ inline float3 sample_texture(const sf::Image &img, float u, float v)
     return float3(temp.r, temp.g, temp.b)/255.0;
 }
 
+inline float3 make_orthonormal_basis(float3 dir, float3 &normal)
+{
+    float3 axis[3];
+    axis[2] = unit(normal);
+    float3 a;
+    if (fabs(axis[2].x) > 0.9) 
+        a = float3(0,1,0);
+    else
+        a = float3(1,0,0);
+    axis[1] = unit(axis[2].cross(a));
+    axis[0] = axis[2].cross(axis[1]);
+    return unit(axis[0]*dir.x + axis[1]*dir.y + axis[2]*dir.z);
+}
+
 struct HitInfo
 {
     float t;
     float3 normal;
     std::shared_ptr<Material> mat;
     float3 colour;
+    float costheta;
     float u, v;
 };
 
 class Material
 {
     public:
-    virtual bool scatter(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) const = 0;
+    virtual bool BSDF(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) const = 0;
     virtual float3 colour(){return float3();}
 };
 
@@ -28,8 +43,8 @@ class Emissive : public Material
 {
     public:
     Emissive(float3 c) : m_colour(c){}
-    bool scatter(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) const;
-    float3 colour() const { return m_colour; }
+    bool BSDF(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) const;
+    float3 colour(){ return m_colour; }
     
     private:
     float3 m_colour;
@@ -39,7 +54,7 @@ class Lambertian : public Material
 {
     public:
     Lambertian(sf::Image t):texture(t) {}
-    bool scatter(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) const;
+    bool BSDF(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) const;
     
     private:
     sf::Image texture;
@@ -49,7 +64,7 @@ class Mirror : public Material
 {
     public:
     Mirror(float3 c, float glossiness = 0.0f): m_colour(c), glossiness(glossiness){}
-    bool scatter(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) const;
+    bool BSDF(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) const;
     
     private:
     float3 m_colour;
@@ -60,7 +75,7 @@ class Mix : public Material
 {
     public:
     Mix(std::shared_ptr<Material> mat1, std::shared_ptr<Material> mat2, float mix) :mat1(mat1), mat2(mat2), mix(mix) {}
-    bool scatter(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) const;
+    bool BSDF(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) const;
     private:
     std::shared_ptr<Material> mat1, mat2;
     float mix;
@@ -70,7 +85,7 @@ class Glass: public Material
 {
     public:
     Glass(float3 c, float glossiness = 0.0f): m_colour(c), glossiness(glossiness){}
-    bool scatter(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) const;
+    bool BSDF(Ray &ray, HitInfo &hit, float3 &attenuation, Ray &new_ray) const;
     
     private:
     float3 m_colour;
